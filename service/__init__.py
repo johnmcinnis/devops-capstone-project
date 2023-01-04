8960167219cc60833f342fbff9c64fd4d18525e8
johnmcinnis <connormcinnis52@gmail.com>
2023-01-04 18:26:18 -0400

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -24,8 +24,6 @@ from service import routes, models  # noqa: F401 E402
 # pylint: disable=wrong-import-position
 from service.common import error_handlers, cli_commands  # noqa: F401 E402
 
-
-
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
 
